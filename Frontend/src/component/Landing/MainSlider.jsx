f4d93e073dda1f08229f14710988d4baeca93c2d
AnwarMohammadi2001 <anwarmohammadi1390@gmail.com>
2025-01-18 09:25:36 +0430

    new commit in today

--- Frontend/src/component/Landing/MainSlider.jsx
+++ Frontend/src/component/Landing/MainSlider.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MainSlider = () => {
   return (
     <div>
-      
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae ab enim, eveniet, corrupti aut qui id repellat ipsa nihil quos dolores commodi mollitia, alias adipisci aliquid vitae delectus accusamus quis!
     </div>
   )
 }
--- Frontend/src/component/Landing/apparel/Apparel.jsx
+++ Frontend/src/component/Landing/apparel/Apparel.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Apparel = () => {
   return (
     <div>
-      cccc
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis dolorem animi fugit alias, voluptatem doloremque error neque quisquam necessitatibus vitae, laboriosam deserunt, facere iste iusto nihil quod minus perferendis rerum?
     </div>
   )
 }
