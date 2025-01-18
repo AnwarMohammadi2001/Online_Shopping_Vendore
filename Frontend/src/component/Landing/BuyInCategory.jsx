2f8b34987ed7b275c84540d010c1b108cad2e6a1
AnwarMohammadi2001 <anwarmohammadi1390@gmail.com>
2025-01-18 09:39:31 +0430

    Make the apparel section

---
+++ Frontend/src/component/Landing/BuyInCategory.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BuyInCategory = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BuyInCategory
---
+++ Frontend/src/component/Landing/RecommendedBuy.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RecommendedBuy = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RecommendedBuy
